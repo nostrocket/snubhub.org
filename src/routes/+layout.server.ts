fac439d80e1389ddff36f279f9d5f4c9a416d04c
pqingshuang <26760878+shaibearary@users.noreply.github.com>
2024-01-31 11:08:06 +0800

    problem: gh-page doesnt work
    .
    
    .
    
    .
    
    .
    
    .

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,1 +1,6 @@
-export const ssr = false
+
+//if pre-render is set to true, then adapter-static fails to compile [routes] properly.
+//we set it to false and also add an index.html fallback to svelte config so that it
+//properly compiles to something that can be served by a static web server like github pages.
+export const prerender = false;
+export const ssr = false;
